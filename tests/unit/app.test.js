3d57f34089015817e243db03091214c8cb5d863a
MajdHomsi <majd.homsi.ca@gmail.com>
2022-05-25 22:49:31 -0400

    improved test coverage

---
+++ tests/unit/app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../../src/app');
+
+describe('404 occurrence test', () => {
+  test('Returns an HTTP 404 response', async () => {
+    const res = await request(app).get('/dummy');
+    expect(res.statusCode).toBe(404);
+  });
+});
