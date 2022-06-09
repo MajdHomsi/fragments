0a529b6c2dca1345bee6186dec8e3f7b9fd70162
MajdHomsi <majd.homsi.ca@gmail.com>
2022-06-08 23:11:52 -0400

    Tests

--- tests/unit/memory.test.js
+++ tests/unit/memory.test.js
@@ -1,75 +1,44 @@
 const {
   writeFragment,
   writeFragmentData,
+  listFragments,
   readFragment,
   readFragmentData,
-} = require('../../src/model/data/memory/index');
+} = require('../../src/model/data/memory');
 
-describe('Memory Tests', () => {
-  describe('writeFragment', () => {
-    test('writeFragment passed', async () => {
-      const fragment = {
-        ownerId: '1111',
-        id: 'ID',
-        created: new Date(),
-      };
-
-      await writeFragment(fragment);
-
-      const data = await readFragment(fragment.id, fragment.ownerId);
-
-      expect(data.created).toEqual(fragment.created);
-    });
+describe('test memory data provider', () => {
+  test('readFragment()', async () => {
+    await writeFragment({ ownerId: '123', id: '1', fragment: 'fragment 1' });
+    const frag = await readFragment('123', '1');
+    expect(frag).toEqual({ ownerId: '123', id: '1', fragment: 'fragment 1' });
+    expect(() => readFragment('123', '333').expect.toThrow());
   });
 
-  describe('writeFragment', () => {
-    test('readFragment passed', async () => {
-      const fragment = {
-        ownerId: '2222',
-        id: 'ID',
-        created: new Date(),
-      };
-      await writeFragment(fragment);
+  test('readFragmentData()', async () => {
+    await writeFragment({ ownerId: '234', id: '1', fragment: 'fragment 1' });
+    await writeFragmentData('234', '1', 'confirming fragment 1 (data)');
 
-      const data = await readFragment(fragment.ownerId, fragment.id);
-
-      expect(data.created).toEqual(fragment.created);
-    });
+    const data = await readFragmentData('234', '1');
+    expect(data).toEqual('confirming fragment 1 (data)');
+    expect(() => readFragmentData('234', '444').expect.toThrow());
   });
 
-  describe('writeFragmentData', () => {
-    test('writeFragmentData passed', async () => {
-      const fragment = {
-        ownerId: '3333',
-        id: 'ID',
-        created: new Date(),
-      };
+  test('listFragments() writeFragment() writeFragmentData()', async () => {
+    await writeFragment({ ownerId: 'ab', id: '1', fragment: 'fragment 1' });
+    await writeFragmentData('ab', '1', 'confirming fragment 1');
 
-      const value = 'writing is being tested';
+    await writeFragment({ ownerId: 'ab', id: '2', fragment: 'fragment 2' });
+    await writeFragmentData('ab', '2', 'confirming fragment 2');
 
-      await writeFragmentData(fragment.ownerId, fragment.id, value);
+    const idNum = await listFragments('ab');
+    expect(Array.isArray(idNum)).toBe(true);
+    expect(idNum).toEqual(['1', '2']);
 
-      const data = await readFragmentData(fragment.ownerId, fragment.id);
-
-      expect(data).toEqual(value);
-    });
-  });
-
-  describe('readFragmentData', () => {
-    test('readFragmentData passed', async () => {
-      const fragment = {
-        ownerId: '4444',
-        id: 'ID',
-        created: new Date(),
-      };
-
-      const value = 'reading is being tested';
-
-      await writeFragmentData(fragment.ownerId, fragment.id, value);
-
-      const data = await readFragmentData(fragment.ownerId, fragment.id);
-
-      expect(data).toEqual(value);
-    });
+    const fragments = await listFragments('ab', true);
+    expect(Array.isArray(fragments)).toBe(true);
+    expect(fragments).toEqual([
+      { ownerId: 'ab', id: '1', fragment: 'fragment 1' },
+      { ownerId: 'ab', id: '2', fragment: 'fragment 2' },
+    ]);
   });
 });
