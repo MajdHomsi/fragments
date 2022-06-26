380534aee7a93f9533c2e1f72c19463a26ca9907
MajdHomsi <majd.homsi.ca@gmail.com>
2022-06-26 19:16:38 -0400

    returns existing frag metadata

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -19,3 +19,14 @@ module.exports = async (req, res) => {
     res.status(500).json(createErrorResponse(500, error.message));
   }
 };
+//GET /fragments/:id/info returns an existing fragment's metadata
+module.exports.getInfo = async (req, res, next) => {
+  const user = req.user;
+  const id = req.params.id;
+  try {
+    const fragment = await Fragment.byId(user, id);
+    res.status(200).json(createSuccessResponse({ fragment }));
+  } catch (error) {
+    next(error);
+  }
+};
